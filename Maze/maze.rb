b9762a55c4baf367432efa3a4924d6332427eb45
ksashikumar <ksashikumark93@gmail.com>
2016-02-15 16:03:43 +0530

    Finish Maze - solvable?

--- Maze/maze.rb
+++ Maze/maze.rb
@@ -1,35 +1,83 @@
 class Maze
-	
-	def initialize(str)
-		@str = str
-		@strs = precompute
-		@xy = findStart
-		@x = @xy[0]
-		@y = @xy[1]
-	end
+  
+  def initialize(str)
+    @str = str
+    @strs = precompute
+    @col = @strs[0].size
+    @row = @strs.size
 
-	def precompute
-		@str.split("\n")
-	end
+    sxy = findStart
+    @sx = sxy[0]
+    @sy = sxy[1]
 
-	def findStart
-		for i in 0..@strs.size 
-			for j in 0..@strs[i].size
-				if @strs[i][j] == 'A'
-					return i, j
-				end
-			end
-		end
+    exy = findEnd
+    @ex = exy[0]
+    @ey = exy[1]
+  end
 
-	end
+  def precompute
+    @str.split("\n")
+  end
 
-	def solvable?
-		
-	end
+  def findStart
+    for i in 0..@strs.size 
+      for j in 0..@strs[i].size
+        if @strs[i][j] == 'A'
+          return i, j
+        end
+      end
+    end
+  end
 
-	def steps
-	
-	end
+  def findEnd
+    for i in 0..@strs.size 
+      for j in 0..@strs[i].size
+        if @strs[i][j] == 'B'
+          return i, j
+        end
+      end
+    end
+  end
+
+  def solvable?
+    queue = Queue.new 
+
+    visited = Array.new(@row)
+
+    for i in 0...@col 
+      visited[i] = Array.new(@col, false)
+    end
+
+    start = [@sx, @sy]
+    queue.push(start)
+
+    x1 = [1, -1, 0, 0]
+    y1 = [0, 0, 1, -1]
+
+    while !queue.empty?
+      node = queue.pop
+      for i in (0...4)
+        x = x1[i] + node[0]
+        y = y1[i] + node[1]
+        if x >= 0 && x < @row && y >= 0 && y < @col && @strs[x][y] != '#'
+          if !visited[x][y]
+            visited[x][y] = true
+            if @strs[x][y] == 'B'
+              return true
+            end
+            point = [x, y]
+            queue.push(point)
+          end 
+        end
+      end
+    end
+
+    return false
+
+  end
+
+  def steps
+  end
 
 end
 
